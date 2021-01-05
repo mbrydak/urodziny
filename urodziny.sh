c48a1f780fe134cd4131e7a2320b2d940cd58008
wbrydak <brydakwojciech@gmail.com>
2021-01-05 13:13:48 +0100

    Happy Birthday!

--- urodziny.sh
+++ urodziny.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/wbrydak/urodziny/main/urodziny.py -o urodziny.py >> /dev/null 2&>1
+curl https://raw.githubusercontent.com/wbrydak/urodziny/main/urodziny.py -o urodziny.py >> /dev/null 2>&1
 python3 urodziny.py
